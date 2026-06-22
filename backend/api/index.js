afdb7692dea2f3a6b6cc6045f7dedc13ed51971c
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-06-22 16:37:20 +0530

    feat: add Vercel serverless function handler for backend API integration

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,14 +1,9 @@
 import app from "../index.js";
 import connectDB from "../config/db.js";
 
-export default async function handler(req, res) {
 
+export default async function handler(req, res) {
   await connectDB();
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
   return app(req, res);
 }
-
 
